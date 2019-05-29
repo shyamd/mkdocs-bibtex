cf8d441495c9d5c3292041c1da94fa2bf5d92180
Shyam Dwaraknath <shyamd@lbl.gov>
2019-05-28 22:10:59 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mkdocs-bibtex",
+    version="0.1.0",
+    description="An MkDocs plugin that enables managing citations with BibTex",
+    keywords="mkdocs python markdown bibtex",
+    url="https://github.com/shyamd/mkdocs-bibtex/",
+    author="Shyam Dwaraknath",
+    author_email="shyamd@lbl.gov",
+    license="BSD",
+    python_requires=">=3.7",
+    install_requires=["mkdocs>=1", "pybtex>=0.22"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
