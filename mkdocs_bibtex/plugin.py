02a8a61e8e0b1b23d0f6fbb02e00417aeea5ca5d
Shyam Dwaraknath <shyamd@lbl.gov>
2019-05-29 11:45:47 -0700

    implement load configuration

--- mkdocs_bibtex/plugin.py
+++ mkdocs_bibtex/plugin.py
@@ -26,3 +26,29 @@ class BibTexPlugin(BasePlugin):
 
     def __init__(self):
         self.bib_data = None
+
+    def on_config(self, config):
+        """
+        Loads bibliography on load of config
+        """
+        config_path = os.path.dirname(config.config_file_path)
+
+        bibfiles = []
+        if config["bib_file"] is not None:
+            bibfiles.append(config["bib_file"])
+        elif config["bib_dir"] is not None:
+            bibfiles.extend(config["bib_dir"])
+        else:
+            raise Exception("Must supply a bibtex file or directory for bibtex files")
+
+        # load bibliography data
+        refs = []
+        for bibfile in bibfiles:
+            if not os.path.isabs(bibfile):
+                bibfile = os.path.abspath(os.path.join(config_path, bibfile))
+                bibdata = parse_file(bib_file)
+                refs.update(bibdata.entries)
+
+        self.bib_data = BibliographyData(entries=refs)
+
+        return config
