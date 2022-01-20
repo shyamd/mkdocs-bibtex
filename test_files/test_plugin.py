4461c8de9a693583ad0856c168aba5f22ed03930
shyamd <16827130+shyamd@users.noreply.github.com>
2022-01-19 16:54:24 -0800

    move tests out of module

---
+++ test_files/test_plugin.py
@@ -0,0 +1,183 @@
+import os
+
+import pytest
+
+from mkdocs_bibtex.plugin import BibTexPlugin
+from mkdocs_bibtex.utils import (find_cite_keys, format_bibliography,
+                                 insert_citation_keys)
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+test_files_dir = os.path.abspath(os.path.join(module_dir, "..", "test_files"))
+
+
+@pytest.fixture
+def plugin():
+    plugin = BibTexPlugin()
+    plugin.load_config(
+        options={"bib_file": os.path.join(test_files_dir, "test.bib")},
+        config_file_path=test_files_dir,
+    )
+    plugin.on_config(plugin.config)
+    return plugin
+
+
+def test_bibtex_loading_bibfile(plugin):
+    assert len(plugin.bib_data.entries) == 3
+
+
+def test_bibtex_loading_bibdir():
+    plugin = BibTexPlugin()
+    plugin.load_config(
+        options={"bib_dir": os.path.join(test_files_dir, "multi_bib")},
+        config_file_path=test_files_dir,
+    )
+
+    plugin.on_config(plugin.config)
+    assert len(plugin.bib_data.entries) == 2
+
+
+@pytest.mark.xfail()
+def test_format_citations(plugin):
+    plugin.csl_file = None
+
+    assert (
+        "[@test]",
+        "@test",
+        "1.",
+        "First Author and Second Author",
+    ) == plugin.format_citations(["@test"])[0]
+
+    # Test arithmatex compatability formatting
+    assert (
+        "[@test2]",
+        "@test2",
+        "1.",
+        "First Author and Second Author\\. Test Title \\(TT\\)\\. *Testing Journal \\(TJ\\)*, 2019",
+    ) == plugin.format_citations(["@test2"])
+
+    plugin.unescape_for_arithmatex = True
+    assert (
+        "[@test2]",
+        "@test2",
+        "1.",
+        "First Author and Second Author\\. Test Title (TT)\\. *Testing Journal (TJ)*, 2019",
+    ) == plugin.format_citations(["@test2"])
+
+    plugin.unescape_for_arithmatex = False
+
+    # Test compound citation
+    assert [
+        (
+            "[@test; @test2]",
+            "@test",
+            "1.",
+            "First Author and Second Author",
+        ),
+        (
+            "[@test; @test2]",
+            "@test2",
+            "2.",
+            "First Author and Second Author\\. Test Title \\(TT\\)\\. *Testing Journal \\(TJ\\)*, 2019",
+        ),
+    ] == plugin.format_citations(["@test; @test2"])
+
+    # test long citation
+
+    plugin.csl_file = None
+    assert (
+        "@Biovort2016",
+        "@Biovort2016",
+        "1",
+        "Benjamin L\\. De Bivort and Bruno Van Swinderen",
+    ) == plugin.format_citations(["Bivort2016"])
+
+    # Test formatting using a CSL style
+    plugin.csl_file = os.path.join(test_files_dir, "nature.csl")
+    assert (
+        "@test",
+        "@test",
+        "1.",
+        "Author, F. & Author, S",
+    ) == plugin.format_citations(["@test"])
+
+    plugin.csl_file = os.path.join(test_files_dir, "nature.csl")
+    assert (
+        "@Biovort2016",
+        "@Biovort2016",
+        "1",
+        "De Bivort, B. L. & Van Swinderen",
+    ) == plugin.format_citations(["Bivort2016"])
+
+    plugin.csl_file = os.path.join(test_files_dir, "springer-basic-author-date.csl")
+    assert ("@test", "@test", "1", "Author F, Author S") == plugin.format_citations(
+        ["@test"]
+    )
+
+
+def test_find_cite_keys():
+    assert find_cite_keys("[@test]") == ["[@test]"]
+    assert find_cite_keys("[@test; @test2]") == ["[@test; @test2]"]
+    assert find_cite_keys("[@test]\n [@test; @test2]") == ["[@test]", "[@test; @test2]"]
+
+
+def test_insert_citation_keys():
+    assert (
+        insert_citation_keys(
+            [
+                (
+                    "[@test]",
+                    "@test",
+                    "1",
+                    "First Author and Second Author",
+                )
+            ],
+            "[@test]",
+        )
+        == "[^1]"
+    )
+
+    assert (
+        insert_citation_keys(
+            [
+                (
+                    "[@test; @test2]",
+                    "@test",
+                    "1",
+                    "First Author and Second Author",
+                ),
+                (
+                    "[@test; @test2]",
+                    "@test2",
+                    "2",
+                    "First Author and Second Author\\. Test Title \\(TT\\)\\. *Testing Journal \\(TJ\\)*, 2019",
+                ),
+            ],
+            "[@test; @test2]",
+        )
+        == "[^1][^2]"
+    )
+
+
+def test_format_bibliography():
+    quads = [
+        (
+            "[@test; @test2]",
+            "@test",
+            "1",
+            "First Author and Second Author",
+        ),
+        (
+            "[@test; @test2]",
+            "@test2",
+            "2",
+            "First Author and Second Author\\. Test Title \\(TT\\)\\. *Testing Journal \\(TJ\\)*, 2019",
+        ),
+    ]
+
+    bib = format_bibliography(quads)
+
+    assert "[^1]: First Author and Second Author" in bib
+    assert (
+        "[^2]: First Author and Second Author\\. Test Title \\(TT\\)\\. *Testing Journal \\(TJ\\)*, 2019"
+        in bib
+    )
