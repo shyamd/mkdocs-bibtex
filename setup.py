3a3c520bd9fa99056c9720d158e8fd782bb6eeee
Shyam Dwaraknath <16827130+shyamd@users.noreply.github.com>
2024-03-03 09:58:06 -0800

    Python 3.12 Support (#232)
    * Add in Python 3.12 to testing
    
    * Update pyproject toml for newer ruff
    
    * Require setuptools for py3.12
    
    * downgrade setuptools for py37

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "pypandoc>=1.5",
         "requests>=2.8.1",
         "validators>=0.19.0",
+        "setuptools>=68.0.0"
     ],
     tests_require=["pytest"],
     packages=find_packages("src"),
