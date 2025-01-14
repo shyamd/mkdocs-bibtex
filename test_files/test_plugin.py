5187f80c7535c195c681fe8ad657d9f91f10a1cf
shyamd <16827130+shyamd@users.noreply.github.com>
2025-01-13 20:13:18 -0800

    remove stale tests

--- test_files/test_plugin.py
+++ test_files/test_plugin.py
@@ -27,9 +27,7 @@ def test_bibtex_loading_bibfile(plugin):
 def test_bibtex_loading_bib_url():
     plugin = BibTexPlugin()
     plugin.load_config(
-        options={
-            "bib_file": "https://raw.githubusercontent.com/shyamd/mkdocs-bibtex/main/test_files/test.bib"
-        },
+        options={"bib_file": "https://raw.githubusercontent.com/shyamd/mkdocs-bibtex/main/test_files/test.bib"},
         config_file_path=test_files_dir,
     )
 
@@ -48,61 +46,5 @@ def test_bibtex_loading_bibdir():
     assert len(plugin.bib_data.entries) == 2
 
 
-def test_on_page_markdown(plugin):
-    """
-    This function just tests to make sure the rendered markdown changees with
-    options and basic functionality works.  It doesn't test "features"
-    """
-    # run test with bib_by_default set to False
-    plugin.config["bib_by_default"] = False
-
-    test_markdown = "This is a citation. [@test]\n\n \\bibliography"
-
-    assert (
-        "[^1]: First Author and Second Author. Test title. *Testing Journal*, 2019."
-        in plugin.on_page_markdown(test_markdown, None, None, None)
-    )
-
-    # ensure there are two items in bibliography
-    test_markdown = "This is a citation. [@test2] This is another citation [@test]\n\n \\bibliography"
-
-    assert "[^2]:" in plugin.on_page_markdown(test_markdown, None, None, None)
-
-    # ensure bib_by_default is working
-    plugin.config["bib_by_default"] = True
-    test_markdown = "This is a citation. [@test]"
-
-    assert "[^1]:" in plugin.on_page_markdown(test_markdown, None, None, None)
-    plugin.config["bib_by_default"] = False
-
-    # ensure nonexistant citekeys are removed correctly (not replaced)
-    test_markdown = "A non-existant citekey. [@i_do_not_exist]"
-
-    assert "[@i_do_not_exist]" in plugin.on_page_markdown(
-        test_markdown, None, None, None
-    )
-
-    # Ensure if an item is referenced multiple times, it only shows up as one reference
-    test_markdown = "This is a citation. [@test] This is another citation [@test]\n\n \\bibliography"
-
-    assert "[^2]" not in plugin.on_page_markdown(test_markdown, None, None, None)
-
-    # Ensure item only shows up once even if used in multiple places as both a compound and lone cite key
-    test_markdown = "This is a citation. [@test; @test2] This is another citation [@test]\n\n \\bibliography"
-
-    assert "[^3]" not in plugin.on_page_markdown(test_markdown, None, None, None)
 
 
-def test_footnote_formatting_config(plugin):
-    """
-    This function tests to ensure footnote formatting configuration is working properly
-    """
-    # Test to make sure the config enforces {number} in the format
-    bad_plugin = BibTexPlugin()
-    bad_plugin.load_config(
-        options={"footnote_format": ""},
-        config_file_path=test_files_dir,
-    )
-
-    with pytest.raises(Exception):
-        bad_plugin.on_config(bad_plugin.config)
