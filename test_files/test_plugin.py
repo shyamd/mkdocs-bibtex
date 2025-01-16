dd0695f52c319a9ba47416da3ad4cbec81470b0f
Jia Wei Kho <57036630+jiaweikho@users.noreply.github.com>
2025-01-16 16:23:27 +0000

    Zotero pagination (#288)
    * Add responses library for mocking requests
    
    * Mock Zotero API and bibtex entries
    
    Instead of setting up a testing environment in Zotero, I have mocked the
    API documented in [Zotero API
    docs](https://www.zotero.org/support/dev/web_api/v3/basics) especially
    with the pagination feature that splits the bibtex into chunks with a
    maximum length that they call 'limit'. In the fixture, the limit is set
    to 25 or the default.
    
    Along with the mock API, I needed a function that can generate n bibtex
    entries to test paginated and non-paginated results.
    
    * Add test loading bibtex from Zotero
    
    To test paginated and non-paginated responses, this test parametrizes
    the mock_zotero_api fixture with API endpoints with 4 and 150 results
    respectively.
    
    * Add function to handle Zotero API requests
    
    Zotero API's "Sorting and Pagination"
    [docs](https://www.zotero.org/support/dev/web_api/v3/basics) explain
    that results are limited to an integer number of results that can
    increase to a maximum of 100 results per request. If there are more
    results that match the search criteria, a "Link" header will be added
    with a `rel=next` link. The `tempfile_from_zotero` function leverages
    the response header and continues requesting the `next` url until all
    results are returned. To prevent runaway conditions, an arbitrary hard
    limit of 999 has been placed.
    
    * Sanitize Zotero API URL before request
    
    To prevent the user from adding problematic query params to the Zotero
    URL, the `sanitize_zotero_query()` function ensures that the requested
    format is bibtex and the limit is set to the maximum permitted by Zotero
    to reduce the total number of requests.
    
    The mocked Zotero API needs to represent these latest changes too.
    
    * Add responses to GHA testing deps

--- test_files/test_plugin.py
+++ test_files/test_plugin.py
@@ -1,11 +1,42 @@
+import collections.abc
 import os
+import random
+import string
 
 import pytest
+import responses
 
 from mkdocs_bibtex.plugin import BibTexPlugin
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 test_files_dir = os.path.abspath(os.path.join(module_dir, "..", "test_files"))
+MOCK_ZOTERO_URL = "https://api.zotero.org/groups/FOO/collections/BAR/items?format=bibtex"
+
+
+@pytest.fixture
+def mock_zotero_api(request: pytest.FixtureRequest) -> collections.abc.Generator[responses.RequestsMock]:
+    zotero_api_url = "https://api.zotero.org/groups/FOO/collections/BAR/items?format=bibtex&limit=100"
+    bibtex_contents = generate_bibtex_entries(request.param)
+
+    limit = 100
+    pages = [bibtex_contents[i : i + limit] for i in range(0, len(bibtex_contents), limit)]
+
+    with responses.RequestsMock() as mock_api:
+        for page_num, page in enumerate(pages):
+            current_start = "" if page_num == 0 else f"&start={page_num * limit}"
+            next_start = f"&start={(page_num + 1) * limit}"
+            mock_api.add(
+                responses.Response(
+                    method="GET",
+                    url=f"{zotero_api_url}{current_start}",
+                    json="\n".join(page),
+                    headers={}
+                    if page_num == len(pages) - 1
+                    else {"Link": f"<{zotero_api_url}{next_start}>; rel='next'"},
+                )
+            )
+
+        yield mock_api
 
 
 @pytest.fixture
@@ -47,6 +78,17 @@ def test_bibtex_loading_bibdir():
     plugin.on_config(plugin.config)
     assert len(plugin.bib_data.entries) == 2
 
+
+@pytest.mark.parametrize(("mock_zotero_api", "number_of_entries"), ((4, 4), (150, 150)), indirect=["mock_zotero_api"])
+def test_bibtex_loading_zotero(mock_zotero_api: responses.RequestsMock, number_of_entries: int) -> None:
+    plugin = BibTexPlugin()
+    plugin.load_config(
+        options={"bib_file": MOCK_ZOTERO_URL},
+        config_file_path=test_files_dir,
+    )
+
+    plugin.on_config(plugin.config)
+    assert len(plugin.bib_data.entries) == number_of_entries
 
 def test_on_page_markdown(plugin):
     """
@@ -106,3 +148,25 @@ def test_footnote_formatting_config(plugin):
 
     with pytest.raises(Exception):
         bad_plugin.on_config(bad_plugin.config)
+
+def generate_bibtex_entries(n: int) -> list[str]:
+    """Generates n random bibtex entries."""
+
+    entries = []
+
+    for i in range(n):
+        author_first = "".join(random.choices(string.ascii_letters, k=8))
+        author_last = "".join(random.choices(string.ascii_letters, k=8))
+        title = "".join(random.choices(string.ascii_letters, k=10))
+        journal = "".join(random.choices(string.ascii_uppercase, k=5))
+        year = str(random.randint(1950, 2025))
+
+        entries.append(f"""
+@article{{{author_last}_{i}}},
+    title = {{{title}}},
+    volume = {{1}},
+    journal = {{{journal}}},
+    author = {{{author_last}, {author_first}}},
+    year = {{{year}}},
+""")
+    return entries
